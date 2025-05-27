da5ddb46a90b446c13d6649b9fa2f0145a82a2e2
Anas Azkoul <anasazkoul@Mac.home>
2025-05-27 21:41:34 +0200

    working on Navbar composite component

---
+++ src/components/composites/Navbar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+} from '@/components/ui/navigation-menu';
+import {Button} from "@/components/ui/button";
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+<NavigationMenu class="min-w-full flex justify-between gap-6 list-none text-white px-6 py-2">
+  <NavigationMenuItem class="mr-auto">
+    <RouterLink :to="{name: 'Home'}">
+      JamForge
+    </RouterLink>
+  </NavigationMenuItem>
+  <NavigationMenuItem>
+    <RouterLink :to="{name: 'Home'}">
+      My Tabs
+    </RouterLink>
+  </NavigationMenuItem>
+  <NavigationMenuItem>
+    <RouterLink :to="{name: 'Home'}">
+      Learn
+    </RouterLink>
+  </NavigationMenuItem>
+  <NavigationMenuItem>
+    <RouterLink :to="{name: 'Home'}">
+      Community
+    </RouterLink>
+  </NavigationMenuItem>
+  <NavigationMenuItem>
+    <RouterLink :to="{name: 'Home'}">
+      Settings
+    </RouterLink>
+  </NavigationMenuItem>
+  <NavigationMenuItem>
+    <Button variant="secondary">New Tab</Button>
+  </NavigationMenuItem>
+  <Avatar>
+    <AvatarFallback class="text-gray-950">AA</AvatarFallback>
+  </Avatar>
+</NavigationMenu>
+</template>
+
+<style scoped>
+
+</style>
